01a437886a21b544b59bc5b456a6bd5d9d66c02a
SebinPrince <sebinpaikada1@gmail.com>
2024-10-04 16:09:58 +0530

    Add files via upload

---
+++ sebin.py
@@ -0,0 +1,26 @@
+'''
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+'''
+'''
+'''
+x="python "
+y="is "
+z="hello "
+print(x+y+z)
+'''
+x=input("enter name is ")
+print("name is",x)
+'''
+'''
+a="hello"
+b="world"
+c=a+b
+print(c)
+'''
+'''
+a="hello world"
+print(a[0:5])
+'''
